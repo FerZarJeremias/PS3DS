bc84f0af0b08d0751a1cecac97ddbab01dd7a57a
Jeremias Fernandez Zalazar <113863733+FerZarJeremias@users.noreply.github.com>
2026-07-02 02:13:30 -0300

    Update memory.h

--- include/core/memory.h
+++ include/core/memory.h
@@ -3,11 +3,14 @@
 
 #include "core/types.h"
 
-#define MAIN_RAM_SIZE (4 * 1024 * 1024)
+typedef struct
+{
+    u8 main_ram[4 * 1024 * 1024];
+} Memory;
 
-void memory_init(void);
+void memory_init(Memory *memory);
 
-u8 memory_read8(u32 addr);
-void memory_write8(u32 addr, u8 value);
+u8 memory_read8(Memory *memory, u32 addr);
+void memory_write8(Memory *memory, u32 addr, u8 value);
 
 #endif
