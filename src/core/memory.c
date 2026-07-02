97d98a71bd8887125fae25b42283bc284d1a09d5
Jeremias Fernandez Zalazar <113863733+FerZarJeremias@users.noreply.github.com>
2026-07-02 00:06:58 -0300

    Update memory.c

--- src/core/memory.c
+++ src/core/memory.c
@@ -1,21 +1,5 @@
-#include <stdlib.h>
+#include <string.h>
 
 #include "core/memory.h"
 
-#define MAIN_RAM_SIZE (4 * 1024 * 1024)
-
-int memory_init(memory_t *memory)
-{
-    memory->ram = (u8*)malloc(MAIN_RAM_SIZE);
-
-    if (memory->ram == NULL)
-        return -1;
-
-    return 0;
-}
-
-void memory_shutdown(memory_t *memory)
-{
-    free(memory->ram);
-    memory->ram = NULL;
-}
+static u8 main_ram[MAIN_RAM_SIZE];
