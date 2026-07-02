8570e421b0f9520aa3c8f4c3a5c65de4571760c2
Jeremias Fernandez Zalazar <113863733+FerZarJeremias@users.noreply.github.com>
2026-07-01 21:29:23 -0300

    Update nds.c

--- scr/core/nds.c
+++ scr/core/nds.c
@@ -2,11 +2,17 @@
 
 int nds_init(nds_t *nds)
 {
+    if (memory_init(&nds->memory) != 0)
+        return -1;
+
     nds->initialized = 1;
+
     return 0;
 }
 
 void nds_shutdown(nds_t *nds)
 {
+    memory_shutdown(&nds->memory);
+
     nds->initialized = 0;
 }
