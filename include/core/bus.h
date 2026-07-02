1ecbe07c213bb5ae05f3ebc59b4c65e4ca6b7588
Jeremias Fernandez Zalazar <113863733+FerZarJeremias@users.noreply.github.com>
2026-07-02 00:19:50 -0300

    Update bus.h

--- include/core/bus.h
+++ include/core/bus.h
@@ -1,24 +1,11 @@
 #ifndef BUS_H
 #define BUS_H
 
-#include <stdint.h>
+#include "core/types.h"
 
-#define MAIN_RAM_SIZE (4 * 1024 * 1024)
+void bus_init(void);
 
-typedef struct
-{
-    uint8_t main_ram[MAIN_RAM_SIZE];
-
-} MemoryBus;
-
-void bus_init(MemoryBus *bus);
-
-uint8_t bus_read8(MemoryBus *bus, uint32_t address);
-uint16_t bus_read16(MemoryBus *bus, uint32_t address);
-uint32_t bus_read32(MemoryBus *bus, uint32_t address);
-
-void bus_write8(MemoryBus *bus, uint32_t address, uint8_t value);
-void bus_write16(MemoryBus *bus, uint32_t address, uint16_t value);
-void bus_write32(MemoryBus *bus, uint32_t address, uint32_t value);
+u8 bus_read8(u32 addr);
+void bus_write8(u32 addr, u8 value);
 
 #endif
