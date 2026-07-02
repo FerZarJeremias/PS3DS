0da94c5be592ec8cc847737ca0f78f4de2262c5f
Jeremias Fernandez Zalazar <113863733+FerZarJeremias@users.noreply.github.com>
2026-07-02 00:11:19 -0300

    Update memory.c

--- src/core/memory.c
+++ src/core/memory.c
@@ -8,3 +8,11 @@ void memory_init(void)
 {
     memset(main_ram, 0, sizeof(main_ram));
 }
+
+u8 memory_read8(u32 addr)
+{
+    if (addr >= MAIN_RAM_SIZE)
+        return 0;
+
+    return main_ram[addr];
+}
